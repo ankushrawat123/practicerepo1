4e9e6ca97764fe3c70f86d36e137085bb4a3e367
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-08 17:36:17 +0530

    uploading factorial file

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter any number for factorial");
+int num = Convert.ToInt32(Console.ReadLine());
+int fact=1;
+for (int i=1;i<=num;i++)
+{
+    fact=fact*i;
+}
+Console.WriteLine("Factorial of {0} is {1}",num,fact);
