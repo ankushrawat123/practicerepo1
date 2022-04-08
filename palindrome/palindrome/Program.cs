25da9976d59bf1faedccbf6494c2ee5feda0d6dc
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-08 18:05:13 +0530

    uploading palindrome file

---
+++ palindrome/palindrome/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter any number to check whether you enter palindrome number or not");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int rev = 0;
+int a = num;
+
+while(num>0)
+{
+    rev = rev * 10 + num % 10;
+    num = num / 10;
+}
+if (a == rev)
+{
+  Console.WriteLine("{0} is a palindrome number", a);
+}
+else
+{ Console.WriteLine("{0} is not a Palindrome number", a); 
+}
